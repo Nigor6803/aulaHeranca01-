21f95d59644ffa6a9ef1c32630bc9f6eecbcccf8
Igor Nascimento <nigor6803@gmail.com>
2024-05-14 11:05:12 -0300

    final de aula

---
+++ src/entidade/Conta.java
@@ -0,0 +1,44 @@
+package entidade;
+
+public class Conta {
+	private Integer conta;
+	private String nome;
+	private Double saldo;
+	
+	public Conta(){
+		}
+	
+	public Conta(Integer conta, String nome,Double salario) {
+		this.conta = conta;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+
+	public Integer getConta() {
+		return conta;
+	}
+
+	public void setConta(Integer conta) {
+		this.conta = conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void retirar(double quantia) {
+		saldo-=quantia;
+	}
+	
+	public void colocar(double quantia) {
+		saldo+=quantia;
+	}
+}
---
+++ src/entidade/ContaEmpresa.java
@@ -0,0 +1,29 @@
+package entidade;
+
+public class ContaEmpresa extends Conta {
+	
+	private Double valorDeEmprestimo;
+		
+	public ContaEmpresa() {
+		super();
+		}
+	
+	public ContaEmpresa(Integer conta,String nome,Double saldo,Double valorDeEmprestimo) {
+		super(conta,nome,saldo);
+		this.valorDeEmprestimo = valorDeEmprestimo;
+	}
+	
+	public double getValorDeEmpresa() {
+		return valorDeEmprestimo;
+	}
+	
+	public void setValorDeEmpresa(Double valorDaEmpresa) {
+		this.valorDeEmprestimo = valorDaEmpresa;
+	}
+	
+	public void emprestimo(double quantidade) {
+		if (quantidade <= valorDeEmprestimo){
+		colocar(quantidade);
+		}
+	}
+}
